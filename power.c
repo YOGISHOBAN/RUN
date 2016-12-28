f505d784d3f8c9cf7a3eda7457ebfdbd342a85dd
YOGISHOBAN <yogishoban1996@gmail.com>
2016-12-28 02:16:41 -0800

    Add files via upload

---
+++ power.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n,power,b;
+	printf("enter the number");
+	scanf("%d",&n);
+	printf("enter the power");
+	scanf("%d",&power);
+b=pow(n,power);
+printf("%d",b);	
+}
