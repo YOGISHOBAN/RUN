11797ddc149ad55cd349179e91df1de8bb820604
YOGISHOBAN <yogishoban1996@gmail.com>
2016-12-27 19:52:47 -0800

    Add files via upload

---
+++ SURYA.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char ch;
+printf("enter the character");
+scanf("%c",&ch);
+if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+{
+printf("it is vowel");
+}
+else
+{
+printf("it is not vowel");
+}
+}
+
