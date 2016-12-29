55b6d66355314e0e06378be1e2a1fc056f85afc1
YOGISHOBAN <yogishoban1996@gmail.com>
2016-12-28 22:37:33 -0800

    Add files via upload

---
+++ dd.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n,temp=0;
+	printf("enter the number");
+	scanf("%d",&n);
+	printf("enter the no");
+	scanf("%d",&temp);
+	if(n%1==0)
+	{
+		
+	}
+		if(n%temp==0){
+		printf("it is prime number");
+	}
+	else
+	{
+		printf("it is not prime number");
+	}
+}
